c67f8001569efb9a59d3c378ad0279bee2b26a62
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2022-05-31 17:35:11 +0400

    added second variant of resolving task 4

--- js/module-6-DOM-task4.js
+++ js/module-6-DOM-task4.js
@@ -1,26 +1,39 @@
 //! 1 variant
 
-const allImageList = document.querySelectorAll('img');
-// console.log(allImageList);
+// const allImageList = document.querySelectorAll('img');
 
-// [...allImageList].filter((el,index, array)=> array.indexOf(el) === index)
+// const filtredImages = [...allImageList]
+//   .flatMap(el => el.src)
+//   .filter((el, index, array) => array.indexOf(el) === index);
 
-const filtredImages = [...allImageList]
-  .flatMap(el => el.src)
-  .filter((el, index, array) => array.indexOf(el) === index);
+// const ol = document.createElement('ol');
+// filtredImages.forEach(el => {
+//   const li = document.createElement('li');
+//   const img = document.createElement('img');
+
+//   li.append(img);
+//   ol.append(li);
+//   img.src = el;
+//   img.width = 100;
+// });
+
+// document.body.innerHTML = '';
+// document.body.append(ol);
+
+//! 2 variant
+
+const listOfImages = document.querySelectorAll('li');
+const sortedListOfImages = [...listOfImages].filter(
+  (el, index, array) =>
+    array.findIndex(
+      findIndexValue => findIndexValue.firstElementChild.src === el.firstElementChild.src,
+    ) === index,
+);
 
 const ol = document.createElement('ol');
-filtredImages.forEach(el => {
-  const li = document.createElement('li');
-  const img = document.createElement('img');
+// console.log(...sortedListOfImages);
 
-  li.append(img);
-  ol.append(li);
-  img.src = el;
-  img.width = 100;
-});
+ol.append(...sortedListOfImages);
 
 document.body.innerHTML = '';
 document.body.append(ol);
-
-//! 2 variant
