621053eaa793cbca01c4875432c3a98e1bd0f400
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2022-06-06 12:41:58 +0400

    adding 2 variant of ToDo List
    add, reser, sort bunnons. Adding new element to array with Templates string.

--- js/module-6-DOM-task9.js
+++ js/module-6-DOM-task9.js
@@ -2,36 +2,73 @@ const refs = {
   input: document.getElementById('add-input'),
   btn: document.getElementById('add-button'),
   btnReset: document.getElementById('reset-button'),
+  btnSort: document.getElementById('sort-button'),
   taskList: document.querySelector('ol'),
   taskArray: [],
 };
 
-refs.btn.addEventListener('click', e => {
-  console.log('Button Add click');
+//! 1 Variant
+// refs.btn.addEventListener('click', e => {
+//   console.log('Button Add click');
 
-  if (refs.input.value !== '') {
-    refs.taskArray.push(refs.input.value);
-    refs.input.value = '';
+//   if (refs.input.value !== '') {
+//     refs.taskArray.push(refs.input.value);
+//     refs.input.value = '';
+//   }
+
+//   makeList(refs.taskArray);
+// });
+
+// refs.btnReset.addEventListener('click', () => {
+//   refs.taskArray = [];
+//   refs.taskList.innerHTML = '';
+// });
+
+// function makeList(arr) {
+//   // console.log(arr);
+//   refs.taskList.innerHTML = '';
+
+//   arr.forEach(element => {
+//     console.log(element);
+
+//     const liEl = document.createElement('li');
+//     liEl.textContent = element;
+//     refs.taskList.append(liEl);
+//   });
+//   document.body.append(refs.taskList);
+// }
+//
+
+//? 2 Variant
+
+const getInputValue = text => `<li>${text}</li>`;
+window.onload = () => {
+  refs.input.focus();
+};
+
+refs.btn.addEventListener('click', () => {
+  if (refs.input.value === '') {
+    return;
   }
-
-  makeList(refs.taskArray);
+  const taskText = getInputValue(refs.input.value);
+  refs.taskList.insertAdjacentHTML('beforeend', taskText);
+  refs.taskArray = refs.taskList.children;
+  refs.input.value = '';
+  refs.input.focus();
 });
 
 refs.btnReset.addEventListener('click', () => {
   refs.taskArray = [];
   refs.taskList.innerHTML = '';
+  refs.input.focus();
 });
 
-function makeList(arr) {
-  // console.log(arr);
+refs.btnSort.addEventListener('click', () => {
+  console.log(refs.taskArray);
+  const sortedTaskArray = [...refs.taskArray].sort((a, b) =>
+    a.innerText.localeCompare(b.innerText),
+  );
+
   refs.taskList.innerHTML = '';
-
-  arr.forEach(element => {
-    console.log(element);
-
-    const liEl = document.createElement('li');
-    liEl.textContent = element;
-    refs.taskList.append(liEl);
-  });
-  document.body.append(refs.taskList);
-}
+  sortedTaskArray.forEach(e => refs.taskList.insertAdjacentHTML('beforeend', e.outerHTML));
+});
