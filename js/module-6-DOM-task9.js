15ca852c1319f54946c4bacbd23c2657b72635f9
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2022-06-06 11:27:54 +0400

    adding ToGolist corrections

--- js/module-6-DOM-task9.js
+++ js/module-6-DOM-task9.js
@@ -1,13 +1,13 @@
 const refs = {
   input: document.getElementById('add-input'),
   btn: document.getElementById('add-button'),
+  btnReset: document.getElementById('reset-button'),
   taskList: document.querySelector('ol'),
   taskArray: [],
 };
 
 refs.btn.addEventListener('click', e => {
   console.log('Button Add click');
-  // console.log(this);
 
   if (refs.input.value !== '') {
     refs.taskArray.push(refs.input.value);
@@ -17,8 +17,13 @@ refs.btn.addEventListener('click', e => {
   makeList(refs.taskArray);
 });
 
+refs.btnReset.addEventListener('click', () => {
+  refs.taskArray = [];
+  refs.taskList.innerHTML = '';
+});
+
 function makeList(arr) {
-  console.log(arr);
+  // console.log(arr);
   refs.taskList.innerHTML = '';
 
   arr.forEach(element => {
